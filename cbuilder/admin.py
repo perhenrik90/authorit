67939bcc10d5a568b8f7e22eaebac8446f4e932e
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2016-02-27 17:43:05 +0100

    updated models and added fluid gui

--- cbuilder/admin.py
+++ cbuilder/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from cbuilder.models import Course, Slide
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Slide)
--- cbuilder/models.py
+++ cbuilder/models.py
@@ -1,8 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Course(models.Model):
 
     title = models.CharField(max_length=26)
     description = models.CharField(max_length=300)
+
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
+
+
+class Slide(models.Model):
+
+    title = models.CharField(max_length=26)
+    modified = models.DateTimeField(auto_now=True)
+
     
+    html = models.CharField(max_length=1500)
+    course = models.ForeignKey(Course)
+
