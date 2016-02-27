1ae0a375827d039d4e8c8e4ed148a99a213aeed0
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2016-02-27 22:39:12 +0100

    added an overview and conected it to the model

--- default/views.py
+++ default/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader, Template, Context
+from django.http import HttpResponse
 
-# Create your views here.
+from cbuilder.models import Course
+
+
+def overview(request):
+
+    c = {}
+
+    c["courses"] = Course.objects.filter(owner=request.user)
+    
+    template = loader.get_template("overview.html")
+    context = RequestContext(request, c)
+    return HttpResponse(template.render(context))
