ee9029003b687e1e1414704b7be22da5d38146b4
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2016-02-27 22:45:07 +0100

    lagt til urler

--- authorit/urls.py
+++ authorit/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'authorit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^overview$', 'default.views.overview', name='overview'),
+    
     url(r'^editslide$', 'cbuilder.views.edit_slide', name='edit slide'),
         
     url(r'^admin/', include(admin.site.urls)),
