2195033e111defb3bc5c7f7919e1bf2d5e61f222
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2016-02-28 19:36:41 +0100

    connected old view to the model

--- export/views.py
+++ export/views.py
@@ -4,11 +4,21 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader, Template, Context
 from django.http import HttpResponse
 
+from cbuilder.models import Course, Slide
 
 def preview(request):
 
     c = {}
 
+    if 'pid' in request.GET:
+        pid = request.GET['pid']
+        course = Course.objects.get(id=pid)
+        c["course"] = course
+        c["slides"] = Slide.objects.filter(course=course)
+        
+    else:
+        c["message"] = _("Course not found!")
+        
     template = loader.get_template("slides.html")
     context = RequestContext(request, c)
     return HttpResponse(template.render(context))
