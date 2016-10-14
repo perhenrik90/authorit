3ddb314d28750564d52955faee67bd06a72ad892
Per-Henrik Kvalnes <per-henrik.kvalnes@nlsh.no>
2016-10-14 10:08:06 +0200

    added unicode on scorm exporter

--- export/tools/scorm.py
+++ export/tools/scorm.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 from django.template import loader, Template, Context
 from django.shortcuts import render, redirect
 from authorit import settings
