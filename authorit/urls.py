c8a20612c1ea67360922140e3b38164114af427c
Per-Henrik <perhenrik.kvalnes@gmail.com>
2017-08-11 15:37:19 +0200

    Update urls

--- authorit/urls.py
+++ authorit/urls.py
@@ -1,24 +1,25 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import  static
 
+import default.views
+import export.views
+
 import settings
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'authorit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'default.views.login_view', name='login'),
-    url(r'^dashboard$', 'default.views.dashboard', name='dashboard'),
+    url(r'^$', default.views.login_view, name='login'),
+    url(r'^dashboard$', default.views.dashboard, name='dashboard'),
 
-    url(r'^view/(?P<project>[\w-]+)/$', 'export.views.preview', name='preview'),
-    url(r'^export_scorm$', 'export.views.export_scorm', name='export_scorm'),
-    url(r'^import_scorm$', 'export.views.import_scorm', name='import_scorm'),
-    url(r'^search$', 'export.views.search_projects', name='search_project'),
-
+    url(r'^view/(?P<project>[\w-]+)/$', export.views.preview, name='preview'),
+    url(r'^export_scorm$', export.views.export_scorm, name='export_scorm'),
+    url(r'^import_scorm$', export.views.import_scorm, name='import_scorm'),
 
     url(r'^course/', include('cbuilder.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+     url(r'^admin/', admin.site.urls),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
